62448873a5d54d4432c39454c63f0fcbb0a82472
MateoBast <mateobastidas146@gmail.com>
2025-10-15 16:17:18 -0300

    Update header.js

--- componentes/header.js
+++ componentes/header.js
@@ -3,9 +3,9 @@ function header(element) {
   componentEl.innerHTML = `    
     <img class="header__logo" src="./imagenes/logo.png" alt="">
     <div>
-        <a class="texto__header" href="http://127.0.0.1:8080/portfolio.html">portafolio</a>
-        <a class="texto__header" href="http://127.0.0.1:8080/servicios.html">servicios</a>
-        <a class="texto__header" href="http://127.0.0.1:8080/contacto.html">contacto</a>
+        <a class="texto__header" href="/portfolio.html">portafolio</a>
+        <a class="texto__header" href="/servicios.html">servicios</a>
+        <a class="texto__header" href="/contacto.html">contacto</a>
     </div>
   `;
   element.appendChild(componentEl);
